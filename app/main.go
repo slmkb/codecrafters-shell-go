36ed209a3dc1895d3d7ec3664d843d193822f02c
Krzysztof <185361376+slmkb@users.noreply.github.com>
2026-01-04 17:15:17 +0100

    added simple echo command

--- app/main.go
+++ app/main.go
@@ -1,33 +1,12 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strings"
 )
 
 // Ensures gofmt doesn't remove the "fmt" import in stage 1 (feel free to remove this!)
 var _ = fmt.Print
 
 func main() {
-	// TODO: Uncomment the code below to pass the first stage
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("$ ")
-		if !scanner.Scan() {
-			break
-		}
-		command := scanner.Text()
-		if strings.Fields(command)[0] == "exit" {
-			break
-		}
-		fmt.Printf("%s: command not found\n", command)
-	}
-
-	if err := scanner.Err(); err != nil {
-		fmt.Fprintln(os.Stderr, "Error reading input:", err)
-		os.Exit(1)
-	}
-
+	repl()
 }
